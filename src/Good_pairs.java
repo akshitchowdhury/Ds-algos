ef5f58d03b5694b390eec6c8d2a0f3f6727453a5
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-12-21 20:07:11 +0530

    p1

--- src/Good_pairs.java
+++ src/Good_pairs.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Good_pairs {
+public class Good_pairs {
+    public static int numIdenticalPairs(int[] nums) {
+
+        int pairs = 0;
+
+        for(int i=0; i<nums.length; i++){
+
+            for(int j= i+1; j<nums.length; j++){
+
+                if(nums[i]==nums[j]){
+
+                    pairs++;
+                }
+            }
+
+        }
+
+        return pairs;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,1,3};
+
+        Good_pairs numIdenticalPairs = new Good_pairs();
+
+        System.out.println(numIdenticalPairs(nums));
+    }
 }

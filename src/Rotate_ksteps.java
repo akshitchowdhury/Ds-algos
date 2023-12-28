4f5af8bb67ee8b827c7381478349319bbf7e89dd
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-12-28 18:39:38 +0530

    ocommit1

--- src/Rotate_ksteps.java
+++ src/Rotate_ksteps.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+
 public class Rotate_ksteps {
+    public void rotate(int[] nums, int k) {
+
+        for(int i=0; i< nums.length;i++){
+
+            int formula = nums[nums.length-k+i];
+
+            int temp = nums[i];
+            nums[i] = nums[formula];
+
+            nums[formula] = temp;
+
+        }
+        Arrays.toString(nums);
+
+    }
 }

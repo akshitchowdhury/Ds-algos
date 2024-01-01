d86317a0572b6ab60b0062754390336185a79ab1
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-01 17:57:57 +0530

    e2

--- src/Even_digits.java
+++ src/Even_digits.java
@@ -1,3 +1,34 @@
 public class Even_digits {
 
+    public static void main(String[] args) {
+
+        int num = 345;
+
+        String str = Integer.toString(num);
+        System.out.println(str.length());
+    }
+
+    public int findNumbers(int[] nums) {
+
+
+        int count =0;
+        for(int i=0; i< nums.length; i++){
+
+            if(nums[i]>10){
+
+                String str = Integer.toString(nums[i]);
+
+                int len = str.length();
+                if(len%2==0 ){
+
+                    count++;
+                }
+
+            }
+
+
+        }
+
+        return count;
+    }
 }

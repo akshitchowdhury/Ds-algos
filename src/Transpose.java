4d249dadd4189bb0e6ebb7a81b687bda85fcb2aa
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-01 18:23:54 +0530

    e4

--- src/Transpose.java
+++ src/Transpose.java
@@ -1,3 +1,21 @@
 public class Transpose {
 
+
+    public int[][] transpose(int[][] matrix) {
+
+            int l1 = matrix.length;
+            int l2=  matrix[0].length;
+
+            int [][] store = new int [l2][l1];
+
+            for(int row=0; row< l1; row++){
+                for(int col = 0; col<l2; col++){
+
+                    store[col][row] = matrix[row][col];
+                }
+            }
+
+
+        return matrix;
+    }
 }

20b7178b5aabdd2cf9cc04b11cce968efa21071c
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-02 19:58:54 +0530

    e6

--- src/Array_integer.java
+++ src/Array_integer.java
@@ -1,14 +1,55 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Array_integer {
 
     public static void main(String[] args) {
 
-        int [] arr = {1,5,7};
-
-        String str = Arrays.toString(arr);
 
 
-        System.out.println(str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+    public List<Integer> addToArrayForm(int[] num, int k) {
+
+        List<Integer> list = new ArrayList<> ();
+
+        StringBuilder joiner = new StringBuilder();
+        for(int value: num){
+            joiner.append(value);
+        }
+
+        String res = joiner.toString();
+
+        int number = Integer.parseInt(res);
+
+        int nres = number+k;
+
+        String farr[] = Integer.toString(nres).split(",");
+
+        for(int i=0; i< farr.length;i++){
+
+            int setter = Integer.parseInt(farr[i]);
+            list.set(i, setter) ;
+        }
+
+
+        return list;
+    }
+
 }

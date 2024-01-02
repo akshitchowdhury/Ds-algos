ee2fb43200032ec51f6f72bf37f654e220915eb1
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-02 17:45:16 +0530

    e6

---
+++ src/Array_integer.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Array_integer {
+
+    public static void main(String[] args) {
+
+        int [] arr = {1,5,7};
+
+        String str = Arrays.toString(arr);
+
+
+        System.out.println(str);
+    }
+}

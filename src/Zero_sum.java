088137c7c082cb7d5a0e1a99aa868b16c50b56a6
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-09 18:37:43 +0530

    r4

--- src/Zero_sum.java
+++ src/Zero_sum.java
@@ -1,5 +1,27 @@
 public class Zero_sum {
     public static void main(String[] args) {
 
+        int rand =(int) Math.random();
+
+        double num = Math.random();
+
+        int whole = (int) Math.round(num*10);
+
+            System.out.println( whole);
+
+    int arr[] = new int[5];
+
+    for(int i =0; i<arr.length; i++){
+
+        arr[i] = whole;
+        System.out.println(arr[i]);
+
+
     }
+
+
+    }
+
+
+
 }

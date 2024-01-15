83539d70a9f14de1a0498451bd9392a6b272f50a
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-15 18:19:36 +0530

    g4

--- problems_2/src/Sqrt.java
+++ problems_2/src/Sqrt.java
@@ -1,11 +1,22 @@
 public class Sqrt {
     public int mySqrt(int x) {
 
+        long r = x;
 
-        return x;
+        while(r*r<x){
+
+            r = (r+x/r)/2;
+
+
+        }
+
+        return (int) x;
     }
 
     public static void main(String[] args) {
 
+        Sqrt sq = new Sqrt();
+
+        sq.mySqrt(25);
     }
 }

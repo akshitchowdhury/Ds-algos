74e7df98613d60c2b75577b54634e91304dc6a9d
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-15 17:47:15 +0530

    g2

--- problems_2/src/Guess_game.java
+++ problems_2/src/Guess_game.java
@@ -4,9 +4,28 @@ public class Guess_game {
     }
 
 
-        public int guessNumber(int n) {
+    public int guessNumber(int n) {
+        int first = 1;
+        int last = n;
 
-        return n;
+        while(first<=last){
+
+            int mid = first + (last-first)/2;
+
+            if(guess(mid)==0){
+                return mid;
+            }
+
+            else if(guess(mid)==-1){
+                last = mid-1;
+            }
+
+            else if (guess(mid)==1){
+                first = mid+1;
+            }
+
         }
+        return -1;
 
-}
+
+    }

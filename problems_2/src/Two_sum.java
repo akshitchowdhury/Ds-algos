4bd43078b65ba26e2495b6793ed048768bc8b416
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-16 19:04:41 +0530

    b2\2

--- problems_2/src/Two_sum.java
+++ problems_2/src/Two_sum.java
@@ -1,2 +1,37 @@
 public class Two_sum {
+    public int[] twoSum(int[] numbers, int target) {
+        int [] stock = new int[2];
+
+        for(int i=0; i<numbers.length; i++){
+            int limit = -1;
+            if(numbers[i]>=target){
+                limit =  i;
+                for(int j=0; j<limit; j++){
+                    for (int k=1; k< limit; k++){
+                        if(numbers[j]+numbers[k]==target){
+                            stock[0] = j+1;
+                            stock[1] = k+1;
+                        }
+                    }
+                }
+            }
+            else{
+                limit = numbers.length;
+                for(int j=0; j<limit; j++){
+                    for (int k=1; k< limit; k++){
+                        if(numbers[j]+numbers[k]==target){
+                            stock[0] = j+1;
+                            stock[1] = k+1;
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+
+        return stock;
+    }
+
 }

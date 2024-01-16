6948891623d9edac08978f91d3bf3f36c9f63782
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-16 18:04:26 +0530

    b2

--- problems_2/src/Bad_version.java
+++ problems_2/src/Bad_version.java
@@ -2,10 +2,26 @@ public class Bad_version {
 
     public int firstBadVersion(int n) {
 
-        int version = -1;
+        int left =0;
+        int right = n;
 
+        while(left<=right){
 
-        return version;
+            int mid = left+(right-left)/2;
+
+            if(isBadVersion(mid)==true && isBadVersion(mid-1)==false){
+                return mid;
+            }
+
+            else if(isBadVersion(mid)==false){
+                left = mid+1;
+            }
+            else{
+                right = mid;
+            }
+
+        }
+        return -1;
 
     }
 }

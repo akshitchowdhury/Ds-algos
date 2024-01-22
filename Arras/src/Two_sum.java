d1434d68476ee8764900c76f93c101f4bf478bc9
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-22 18:45:44 +0530

    s2

--- Arras/src/Two_sum.java
+++ Arras/src/Two_sum.java
@@ -3,4 +3,25 @@ public class Two_sum {
     public static void main(String[] args) {
 
     }
+    public int[] twoSum(int[] numbers, int target) {
+
+        int [] store = new int[2];
+
+        int l = 0;
+        int h= numbers.length-1;
+
+        while(l<h){
+
+            int res = target - numbers[l];
+
+            if(res == numbers[h]&& res!= numbers[l]){
+                store[0] = l+1;
+                store[1] = h+1;
+            }
+
+        }
+
+
+        return store;
+    }
 }
